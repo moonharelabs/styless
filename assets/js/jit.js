af456d326551d5d380a6f6391ab609d0c8eafc95
Kavindu Santhusa <63494497+Ksengine@users.noreply.github.com>
2021-05-01 13:54:08 +0530

    Update jit.js

--- assets/js/jit.js
+++ assets/js/jit.js
@@ -1,90 +1,9 @@
-"use strict";
-
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-(function (window) {
-  var moonHare = {};
-  moonHare.variants = {};
-  moonHare.plugins = {};
-  moonHare.filters = [];
-  moonHare.styleEl = window.document.createElement('style');
-  moonHare.styleEl.id = 'MOONHARE_STYLE_ELEMENT'; // Append <style> element to <head>
-
-  window.document.head.appendChild(moonHare.styleEl); // @copyright - jQuery(https://tldrlegal.com/license/mit-license)
-  // @reference https://github.com/jquery/jquery/blob/main/src/selector/escapeSelector.js
-
-  moonHare.escapeSelector = function (sel) {
-    var rcssescape = /([\0-\x1f\x7f]|^-?\d)|^-$|[^\x80-\uFFFF\w-]/g;
-
-    function fcssescape(ch, asCodePoint) {
-      if (asCodePoint) {
-        if (ch === "\0") {
-          return "\uFFFD";
-        }
-
-        return ch.slice(0, -1) + "\\" + ch.charCodeAt(ch.length - 1).toString(16) + " ";
-      }
-
-      return "\\" + ch;
-    }
-
-    return (sel + "").replace(rcssescape, fcssescape);
-  };
-
-  moonHare.defaultVariant = function (parts) {
-    if (parts.length === 1) {
-      Object.keys(this.plugins).forEach(function (pluginName) {
-        if (parts[0].startsWith(pluginName)) return this.plugins[pluginName].call(this, parts);
-      }, this);
-      return ['', ''];
-    }
-
-    if (parts.length === 2) {
-      return ['.' + this.escapeSelector(parts.join(':')), "{".concat(parts[0], ": ").concat(parts[1], ";}")];
-    }
-
-    return ['', ''];
-  };
-
-  moonHare.addStyles = function (styles) {
-    this.styleEl.innerHTML = styles;
-  };
-
-  moonHare.clearStyles = function () {
-    this.styleEl.innerHTML = '';
-  };
-
-  moonHare.generateStyles = function () {
-    var classList = [];
-
-    _toConsumableArray(window.document.querySelectorAll('*')).forEach(function (el) {
-      return classList = [].concat(_toConsumableArray(classList), _toConsumableArray(el.classList));
-    });
-
-    var styleSheet = classList.map(function (cls) {
-      var parts = cls.split(':');
-      return (this.variants[parts[0]] || this.defaultVariant).call(this, parts);
-    }, this);
-    this.filters.forEach(function (filter) {
-      styleSheet = filter.call(this, styleSheet);
-    }, this);
-    return styleSheet.flat().join('');
-  };
-
-  moonHare.start = function () {
-    this.addStyles(this.generateStyles());
-  };
-
-  if (window.document.readyState != 'loading') moonHare.start();else window.document.addEventListener('DOMContentLoaded', function(){moonHare.start()});
-  window.moonHare = moonHare;
-})(window);
+(function(g){var d={u:function(a){a=a||{};for(var c=1;c<arguments.length;c++){var b=arguments[c];if(b)for(var e in b)b.hasOwnProperty(e)&&(a[e]="object"===typeof b[e]?1==b[e]instanceof Array?b[e].slice(0):this.u(a[e],b[e]):b[e])}return a},g:function(a,c){return function(b,e){return this.l(b.slice(1),a+e+c)}},j:function(a){return function(c,b){var e=this.l(c.slice(1),b);e&&(e[1]=e[0]+"{"+e[1]+"}",e[0]=a);return e}},N:function(a,c){return function(b){return a+":"+b.join(c)}}};d.h=d.u({D:"class",B:{s:{O:"640px",
+L:"768px",K:"1024px",P:"1280px",R:"1536px"}},o:"first last odd even visited checked group-hover group-focus focus-within hover focus focus-visible active disabled dark sm md lg xl xxl".split(" "),i:{first:d.g("",":first"),last:d.g("",":last"),odd:d.g("",":nth-child(odd)"),even:d.g("",":nth-child(even)"),visited:d.g("",":visited"),checked:d.g("",":checked"),"group-hover":d.g(".group:hover ",""),"group-focus":d.g(".group:focus ",""),"focus-within":d.g("",":focus-within"),hover:d.g("",":hover"),focus:d.g("",
+":focus"),"focus-visible":d.g("",":focus-visible"),active:d.g("",":active"),disabled:d.g("",":disabled")},plugins:{object:function(a){return["fill","contain","cover","none","scale"].indexOf(a[0])?"object-fit:"+a.join("-"):"object-position:"+a.join(" ")},overflow:function(a){return"overflow:"+a.join(" ")},"overflow-x":function(a){return"overflow-x:"+a.join(" ")},"overflow-y":function(a){return"overflow-y:"+a.join(" ")},z:function(a){return"z-index:"+a[0]},visible:function(){return"visibility:visible"},
+invisible:function(){return"visibility:hidden"},text:function(a){return(["left","center","right","justify"].indexOf(a[0])?"text-align:":"color:")+a[0]}}},g.I||{});d.cache={};Object.keys(d.h.B.s).forEach(function(a){var c=d.h,b=c.B,e=b.s[a],f=e.replace(parseFloat(e),parseFloat(e)-.1);b=(b=b.s[c.o[c.o.indexOf(a)+1]])?b.replace(parseFloat(b),parseFloat(b)-.1):void 0;c.i[a]||(c.i[a]=d.j("@media(min-width:breakpoint)".replace("breakpoint",e)));c.i["<"+a]||(c.i["<"+a]=d.j("@media(max-width:breakpoint)".replace("breakpoint",
+f)));!c.i["@"+a]&&b&&(c.i["@"+a]=d.j("@media(min-width:breakpoint) and (max-width:next)".replace("breakpoint",e).replace("next",b)))});d.h.i.J="media"===d.h.D?d.j("@media(prefers-color-scheme:dark)"):function(a,c){var b=this.l(a.slice(1),c);b[0]=".dark "+b[0];return b};d.m=document.createElement("style");d.m.id="MOONHARE_STYLE_ELEMENT";d.m.innerHTML=" ";document.head.appendChild(d.m);d.styleSheet=d.m.sheet;d.G=function(a){return(a+"").replace(/([\0-\x1f\x7f]|^-?\d)|^-$|[^\x80-\uFFFF\w-]/g,function(c,
+b){return b?"\x00"===c?"\ufffd":c.slice(0,-1)+"\\"+c.charCodeAt(c.length-1).toString(16)+" ":"\\"+c})};d.F=function(a,c){if(1===a.length){for(var b=a[0].split("-"),e=0;e<b.length;e++)for(var f in this.h.plugins)if(b.slice(0,b.length-e).join("-")===f)return[c,this.h.plugins[f].call(this,b.slice(b.length-e),a[0])];e=a[0].split("[");if(2===e.length&&CSS.supports(e[0].slice(0,-1),e[1].slice(0,-1).replace(/\|/g," ")))return[c,e[0].slice(0,-1)+":"+e[1].slice(0,-1).replace(/\|/g," ")+";"];if(CSS.supports(b.slice(0,
+-1),b.slice(-1).replace(/\|/g," ")))return[c,b.slice(0,-1)+":"+b.slice(-1).replace(/\|/g," ")+";"]}};d.v=function(){[].forEach.call(g.document.querySelectorAll("*"),function(a){[].forEach.call(a.classList,function(c){this.A(c)},this)},this)};d.C=function(a,c,b,e){Object.keys(this.cache).forEach(function(f){var h=f.split(":");b.forEach(function(k,l){this.h.o.indexOf(k)>this.h.o.indexOf(h[l])&&(this.cache[e]=this.cache[f],this.styleSheet.addRule(a,c,this.cache[f]),this.cache[f]+=1)},this)},this);this.styleSheet.addRule(a,
+c,this.styleSheet.cssRules.length);this.cache[e]=this.styleSheet.cssRules.length};d.l=function(a,c){return(this.h.i[a[0]]||this.F).call(this,a,c)};d.A=function(a){if(!(a in this.cache)){var c=a.split(":"),b=this.l(c,"."+this.G(a));b&&this.C(b[0],b[1],c,a)}};d.H=function(){var a=this;"function"==typeof MutationObserver&&(new MutationObserver(function(c){c.forEach(function(b){[].forEach.call(b.target.classList,function(e){a.A(e)})})})).observe(document.documentElement,{attributes:!0,attributeFilter:["class"],
+subtree:!0,childList:!0})};d.start=function(){var a=this;"loading"!=g.document.readyState?this.v():g.document.addEventListener("DOMContentLoaded",function(){return a.v()})};g.M=d;d.start();d.H()})(window);
